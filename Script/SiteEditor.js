5b5ac935204d3da4a97c04be278ad9073a3568ff
SINtyanneru <irjitgd@gmail.com>
2025-11-02 19:35:39 +0900

    ファイル分離

---
+++ Script/SiteEditor.js
@@ -0,0 +1,102 @@
+async function open_site_editor(id) {
+	//初期化
+	mel.site_editor.parent.dataset.id = id;
+	mel.site_editor.data_list.replaceChildren();
+
+	const r = await get_site(id);
+	const crypt_key = key_list[r.site.KEY_ID];
+
+	//名前
+	mel.site_editor.name.value = r.site.NAME;
+
+	//ホスト名
+	mel.site_editor.host.value = r.site.HOST.join("\n");
+
+	//データ
+	for (let i = 0; i < r.data.length; i++) {
+		const data = r.data[i];
+		const iv = decode_base64(data.IV);
+		const contents_encrypt = decode_base64(data.CONTENTS);
+		const contents_plain = await decrypt_text(crypt_key, iv, contents_encrypt);
+
+		mel.site_editor.data_list.appendChild(gen_site_editor_data_field(data.NAME, contents_plain));
+	}
+
+	mel.site_editor.parent.style.display = "block";
+}
+
+function site_editor_add_data() {
+	mel.site_editor.data_list.appendChild(gen_site_editor_data_field("", ""));
+}
+
+function gen_site_editor_data_field(name, text) {
+	let tr = document.createElement("TR");
+
+	//タイプ名
+	let name_td = document.createElement("TD");
+	name_td.className = "NAME";
+	tr.appendChild(name_td);
+
+	let name_input = document.createElement("INPUT");
+	name_input.value = name;
+	name_td.appendChild(name_input);
+
+	//入力欄
+	let input_td = document.createElement("TD");
+	input_td.className = "CONTENTS";
+	tr.appendChild(input_td);
+
+	let input_el = document.createElement("INPUT");
+	input_el.value = text;
+	input_td.appendChild(input_el);
+
+	//削除ボタン
+	let remove_td = document.createElement("TD");
+	tr.appendChild(remove_td);
+
+	let remove_button =document.createElement("BUTTON");
+	remove_button.innerText = "X";
+	remove_button.addEventListener("click", (e)=>{
+		tr.remove();
+	});
+	remove_td.appendChild(remove_button);
+
+	return tr;
+}
+
+async function site_editor_apply() {
+	const id = mel.site_editor.parent.dataset.id;
+
+	const r = await get_site(id);
+	const crypt_key = key_list[r.site.KEY_ID];
+
+	//データ
+	let data_list = [];
+	const data_field_list = mel.site_editor.data_list.childNodes;
+	for (let i = 0; i < data_field_list.length; i++) {
+		const el = data_field_list[i];
+		const name_el = el.querySelector(".NAME");
+		const contents_el = el.querySelector(".CONTENTS");
+		if (name_el == null || contents_el == null) continue;
+
+		const name = name_el.childNodes[0].value;
+		const contents = contents_el.childNodes[0].value;
+		const encrypted = await encrypt_text(crypt_key, contents);
+		data_list.push({
+			"NAME": name,
+			"IV": encode_base64(encrypted.iv),
+			"CONTENTS": encode_base64(encrypted.encrypt)
+		});
+	}
+	await edit_site_data(id, data_list);
+
+	//名前
+	if (r.site.NAME !== mel.site_editor.name.value) {
+		await edit_site_name(id, mel.site_editor.name.value);
+	}
+
+	//ホスト名
+	await edit_site_host(id, mel.site_editor.host.value.split("\n"));
+
+	rspa.open_url(new URL(`${window.location.protocol}//${window.location.hostname}/site/${id}`));
+}
---
+++ Script/SiteViewer.js
@@ -0,0 +1,147 @@
+async function open_site_viewer(id) {
+	//初期化
+	mel.site_viewer.parent.dataset.id = id;
+	mel.site_viewer.data_list.replaceChildren();
+
+	const r = await get_site(id);
+	const crypt_key = key_list[r.site.KEY_ID];
+
+	//名前とか
+	mel.site_viewer.name.innerText = r.site.NAME;
+
+	//データリストにデータを
+	for (let i = 0; i < r.data.length; i++) {
+		const data = r.data[i];
+		if (data.NAME.startsWith("__")) continue;
+
+		const iv = decode_base64(data.IV);
+		const contents_encrypt = decode_base64(data.CONTENTS);
+		const contents_plain = await decrypt_text(crypt_key, iv, contents_encrypt);
+
+		let item = document.createElement("DETAILS");
+		item.className = "DATA_ITEM";
+
+		let name_el = document.createElement("SUMMARY");
+		name_el.innerText = data.NAME;
+		item.appendChild(name_el);
+
+		let contents_el = document.createElement("INPUT")
+		contents_el.setAttribute("readonly", "");
+		contents_el.value = contents_plain;
+		item.appendChild(contents_el);
+
+		let copy_button = document.createElement("BUTTON");
+		copy_button.innerText = "コ";
+		copy_button.addEventListener("click", (e)=>{
+			if (navigator.clipboard == null) return;
+			navigator.clipboard.writeText(contents_plain);
+		});
+		item.appendChild(copy_button);
+
+		mel.site_viewer.data_list.appendChild(item);
+	}
+
+	//TOTP
+	if (r.data.some(x=>x.NAME === "__TOTP")) {
+		mel.site_viewer.totp.enable.parent.style.display = "block";
+		mel.site_viewer.totp.disable.parent.style.display = "none";
+
+		//解読する
+		const encrypt_data = r.data.find(x=>x.NAME === "__TOTP");
+		const plain_data = await decrypt_text(crypt_key, decode_base64(encrypt_data.IV), decode_base64(encrypt_data.CONTENTS));
+		const totp_data = JSON.parse(plain_data);
+
+		//TOTP生成
+		let counter = Math.floor(Date.now() / 1000 / totp_data.period);
+
+		//カウントをビッグエンディアンに変換
+		const counter_byte = new Uint8Array(8);
+		for (let i = 7; i >= 0; i--) {
+			counter_byte[i] = counter & 0xFF;
+			counter >>= 8;
+		}
+
+		//鍵をCryptKeyに変換
+		const key = await crypto.subtle.importKey(
+			"raw",
+			decode_base64(totp_data.key),
+			{name: "HMAC", hash:"SHA-1"},
+			false,
+			["sign"]
+		);
+
+		//HMAC計算
+		const hmac = new Uint8Array(await crypto.subtle.sign("HMAC", key, counter_byte));
+
+		//うんちもみもみ
+		const offset = hmac[hmac.length - 1] & 0x0F;
+		const binary =
+			((hmac[offset] & 0x7F) << 24)     |
+			((hmac[offset + 1] & 0xFF) << 16) |
+			((hmac[offset + 2] & 0xFF) << 8)  |
+			(hmac[offset + 3] & 0xFF);
+
+		//もんだうんちをきれいにするお！
+		const code = (binary % 10 ** totp_data.digits).toString().padStart(totp_data.digits, "0");
+		mel.site_viewer.totp.enable.code.innerText = code;
+	} else {
+		mel.site_viewer.totp.disable.parent.style.display = "block";
+		mel.site_viewer.totp.enable.parent.style.display = "none";
+	}
+
+	mel.site_viewer.parent.style.display = "block";
+}
+
+async function totp_setting() {
+	const id = mel.site_viewer.parent.dataset.id;
+	const r = await get_site(id);
+	const crypt_key = key_list[r.site.KEY_ID];
+
+	const q = await dialog.INPUT("TOTPの鍵またはURLをください。", {TYPE:"TEXT", NAME:""});
+	if (q == null || q == "") return;
+
+	let key = null;
+	try {
+		const url = new URL(q);
+		if (url.protocol.toUpperCase() !== "OTPAUTH:" || url.searchParams.get("secret") == null) {
+			dialog.DIALOG("不正な入力値");
+			return;
+		}
+
+		key = url.searchParams.get("secret").toUpperCase();
+	} catch(ex) {
+		key = q.replace(/[^A-Za-z0-9]/g, "");
+		key = key.toUpperCase();
+	}
+
+	//Base32をUnit8Array化
+	const key_byte = decode_base32(key);
+
+	//データ化して暗号化
+	const totp_data = {
+		key: encode_base64(key_byte),
+		algorithm: "SHA-1",
+		digits: 6,
+		period: 30
+	};
+	const encrypt_data = await encrypt_text(crypt_key, JSON.stringify(totp_data));
+
+	//登録
+	let data = r.data;
+	data.push(
+		{
+			"NAME": "__TOTP",
+			"IV": encode_base64(encrypt_data.iv),
+			"CONTENTS": encode_base64(encrypt_data.encrypt)
+		}
+	);
+	await edit_site_data(id, data);
+}
+
+async function jump_site_editor() {
+	const match = window.location.pathname.match(/\/site\/([A-Za-z0-9-]+)/);
+	if (match == null) return;
+
+	const id = match[1];
+	rspa.open_url(new URL(`${window.location.protocol}//${window.location.hostname}/edit/${id}`));
+}
